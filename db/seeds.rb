8b74f38ab81145bc544ae139d4ff2f771da48d27
github username <github email address>
2019-07-29 15:55:06 -0400

    example test seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  photo1 = Photo.create(caption:"This photo is awesome",img:"",comment:"hi",vote:1)
