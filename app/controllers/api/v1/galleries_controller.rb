9d94c7be2a5c106666ec0659f14bc528f4c53e83
github username <github email address>
2019-08-02 14:39:27 -0400

    now

--- app/controllers/api/v1/galleries_controller.rb
+++ app/controllers/api/v1/galleries_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::GalleriesController < ApplicationController
 
     def index
-    galleries = Gallery.all
-    render json: galleries, status: :ok
+      galleries = Gallery.all
+      render json: galleries, status: :ok
+    end
+
+    def create
+      byebug
+      gallery = Gallery.new
+
     end
 
 
